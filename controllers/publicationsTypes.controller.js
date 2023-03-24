c4157cc59e65bb6e6a9d94ea0e658cba3dbe09c5
JorSchmitz <jorsch.pl@gmail.com>
2023-03-23 22:38:15 -0500

    feat: publicationsTypes endpoint added

---
+++ controllers/publicationsTypes.controller.js
@@ -0,0 +1,60 @@
+const PublicationsTypesService = require('../services/publicationsTypes.service')
+const { getPagination } = require('../utils/helpers')
+
+const publicationsTypesService = new PublicationsTypesService()
+
+const getPublicationsTypes = async (req, res, next) => {
+  const result = {
+    results: {},
+  }
+  const { publicationsTypesPerPage, currentPage } = {
+    publicationsTypesPerPage: 10,
+    currentPage: 1,
+  }
+  const { limit, offset } = getPagination(currentPage, publicationsTypesPerPage)
+
+  try {
+    const publicationsTypes = await publicationsTypesService.findAndCount({
+      ...req.query,
+      limit,
+      offset,
+    })
+    result.results.count = publicationsTypes.count
+    result.results.totalPages = Math.ceil(
+      publicationsTypes.count / publicationsTypesPerPage
+    )
+    result.results.currentPage = currentPage
+    result.results.results = publicationsTypes.rows
+    return res.json(result)
+  } catch (error) {
+    next(error)
+  }
+}
+
+const getPublicationTypeById = async (req, res, next) => {
+  const id = req.params.id
+  const result = { result: {} }
+  try {
+    result.result = await publicationsTypesService.getPublicationTypeById(id)
+    return res.json(result)
+  } catch (error) {
+    next(error)
+  }
+}
+
+const putPublicationTypeById = async (req, res, next) => {
+  const id = req.params.id
+  const obj = req.body
+  try {
+    await publicationsTypesService.updatePublicationTypeById(id, obj)
+    return res.json({ message: 'Success Update' })
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports = {
+  getPublicationTypeById,
+  putPublicationTypeById,
+  getPublicationsTypes,
+}
