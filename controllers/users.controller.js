6b14a015bc035c9733cc8fbb46d9155ec151b8c4
JorSchmitz <jorsch.pl@gmail.com>
2023-03-23 21:26:18 -0500

    fix: users/:id -PUT authorizations

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -45,64 +45,62 @@ const getUserById = async (req, res, next) => {
   }
 }
 
-
 const putUser = async (req, res, next) => {
-	try {
-		const { id } = req.params;
-		const { body } = req;
-		let user = await usersService.updateUser(id, body);
+  try {
+    const { id } = req.params
+    const { body } = req
 
-		// Verify if the user trying to edit is the same as the user being edited
-		if (req.user.id !== user.id) {
-			return res.status(401).json({ error: 'Unauthorized' });
-		}else{
-
-      // Non-editable fields
-      const nonEditableFields = [
-        'token',
-        'email_verified',
-        'password',
-        'email',
-        'username',
-      ];
-  
-      // Editable fields
-      const editableFields = [
-        'first_name',
-        'last_name',
-        'country_id',
-        'code_phone',
-        'phone',
-        'interests',
-      ];
-      // Check if any non-editable fields are being edited
-      const invalidFields = Object.keys(body).filter((field) =>
-      nonEditableFields.includes(field)
-      );
-      if (invalidFields.length > 0) {
-        return res.status(400).json({
-          error: `The following fields are not editable: ${invalidFields.join(
-            ', '
-            )}`,
-          });
-        }
-      
-        // Check if any invalid fields are being edited
-        const validFields = Object.keys(body).filter((field) =>
-        editableFields.includes(field)
-        );
-        if (validFields.length === 0) {
-          return res.status(400).json({
-            error: 'You must provide at least one valid field to edit',
-          });
-        }
+    // Verify if the user trying to edit is the same as the user being edited
+    if (req.user.id !== id) {
+      return res.status(401).json({ error: 'Unauthorized' })
+    } else {
+      let user = await usersService.updateUser(id, body)
     }
-		return res.json({ message: 'Success Update' });
-	} catch (error) {
-		next(error);
-	}
-};
 
+    // Non-editable fields
+    const nonEditableFields = [
+      'token',
+      'email_verified',
+      'password',
+      'email',
+      'username',
+    ]
+
+    // Editable fields
+    const editableFields = [
+      'first_name',
+      'last_name',
+      'country_id',
+      'code_phone',
+      'phone',
+      'interests',
+    ]
+    // Check if any non-editable fields are being edited
+    const invalidFields = Object.keys(body).filter((field) =>
+      nonEditableFields.includes(field)
+    )
+    if (invalidFields.length > 0) {
+      return res.status(400).json({
+        error: `The following fields are not editable: ${invalidFields.join(
+          ', '
+        )}`,
+      })
+    }
+
+    // Check if any invalid fields are being edited
+    const validFields = Object.keys(body).filter((field) =>
+      editableFields.includes(field)
+    )
+    if (validFields.length === 0) {
+      return res.status(400).json({
+        error: 'You must provide at least one valid field to edit',
+      })
+    }
+    return res.json({ message: 'Success Update' })
+  } catch (error) {
+    next(error)
+  }
+}
 
 module.exports = {
   getUsers,
